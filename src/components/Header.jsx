57e373c573bb94c8997853b751eb2bafde5e2961
sweller <shaun.weller@gmail.com>
2023-04-10 10:38:15 -0600

    creating header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Typography, Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const Header = ({ title, subtitle }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h3"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ m: "0 0 5px 0" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.grey[300]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
