51a3c75843108c561583b89bd96d643ecd6a00c8
sweller <shaun.weller@gmail.com>
2023-04-10 11:09:01 -0600

    changes to head and footer background color on data grid

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -85,7 +85,7 @@ const Team = () => {
             color: colors.greenAccent[300],
           },
           "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: colors.blueAccent[600],
+            backgroundColor: colors.primary[700],
             borderBottom: "none",
           },
           "& .MuiDataGrid-virtualScroller": {
@@ -93,7 +93,7 @@ const Team = () => {
           },
           "& .MuiDataGrid-footerContainer": {
             borderTop: "none",
-            backgroundColor: colors.blueAccent[700],
+            backgroundColor: colors.primary[700],
           },
           "& .MuiCheckbox-root": {
             color: `${colors.greenAccent[200]} !important`,
